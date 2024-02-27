58649331c54ba5bf11c1bf5f51bd8801815a2744
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-27 15:06:49 +0100

    add style

--- login/src/app/page.tsx
+++ login/src/app/page.tsx
@@ -4,21 +4,19 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 export default function Home() {
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh'}}>
+    <form className="formulaire">
+     <p className="fs-1 fw-bold text-center mb-5">Login</p>
+      <div className="mb-5">
+        <input type="email" className="form-control" placeholder ="Username" id="exampleInputEmail1" aria-describedby="emailHelp"/>
       </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1"/>
+      <div className="mb-5">
+        <input type="password" placeholder="Password" className="form-control" id="exampleInputPassword1"/>
       </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
+      <div className="d-flex justify-content-center "><button type="submit" className="btn btn-primary w-100 ">Login</button></div>
     </form>
+  </div>
+  
   );
 }
