2dd98844a96116075d742cf42847edadcb6abd77
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-01 11:11:38 +0100

    add file Login

---
+++ login/src/app/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <>
+            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+                <form className="formulaire">
+                    <p className="fs-1 fw-bold text-center mb-5">Login</p>
+                    <div className="mb-5">
+                        <input type="email" className="form-control" placeholder="Username" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-5">
+                        <input type="password" placeholder="Password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div className="d-flex justify-content-center ">
+                        <button type="submit" className="btn btn-primary w-100 ">Login</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Login
--- login/src/app/page.tsx
+++ login/src/app/page.tsx
@@ -1,24 +1,14 @@
 import Image from "next/image";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import Login from '../app/Login'
 
 
 export default function Home() {
   return (
 
-    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh'}}>
-    <form className="formulaire">
-     <p className="fs-1 fw-bold text-center mb-5">Login</p>
-      <div className="mb-5">
-        <input type="email" className="form-control" placeholder ="Username" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-      </div>
-      <div className="mb-5">
-        <input type="password" placeholder="Password" className="form-control" id="exampleInputPassword1"/>
-      </div>
-      <div className="d-flex justify-content-center ">
-        <button type="submit" className="btn btn-primary w-100 ">Login</button>
-        </div>
-    </form>
-  </div>
-  
+    <>
+     <Login/>
+    </>
+
   );
 }
